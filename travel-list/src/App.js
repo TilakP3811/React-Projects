b44d74f4591f63291591b61362e064a105d21f3c
Tilak <patidartilak100@gmail.com>
2023-08-07 13:26:17 +0530

    break one for this application

---
+++ travel-list/src/App.js
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Bags", quantity: 23, packed: true },
+];
+
+const App = () => {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <State />
+    </div>
+  );
+};
+
+const Logo = () => {
+  return <h1>🌴 Far Away 💼</h1>;
+};
+
+const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const State = () => {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have already X items on your list, and you already packed X (X%)
+      </em>
+    </footer>
+  );
+};
+
+export default App;
