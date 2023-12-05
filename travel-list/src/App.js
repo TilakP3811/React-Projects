30b007f18008ccfd8b36a125b0c8931cb65baae4
Tilak Patidar <patidartilak100@gmail.com>
2023-12-05 14:09:53 -0600

    remove extra stuffs

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Bags", quantity: 23, packed: true },
-];
-
 const App = () => {
   const [items, setItems] = useState([]);
 
